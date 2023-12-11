3a4208ef02457c7eb57752edba3adb28b04d0ff5
tomaszjakubbiel <biel535@gmail.com>
2023-12-11 10:20:44 +0100

    first commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
+import React, {useState} from 'react'
+
+
+function navbar() {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo"></Link>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default navbar
